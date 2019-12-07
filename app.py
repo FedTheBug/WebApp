9210eb90d1adefb6bc54f6e8d2d9c8af8737e051
FedTheBug <fahadmuntasir747@gmail.com>
2019-12-07 06:51:18 +0600

    Initial Commit

--- app.py
+++ app.py
@@ -1,27 +1,55 @@
 import os
+from datetime import datetime
 from flask import Flask, render_template, url_for, request, jsonify, flash, redirect
 from forms import RegistrationForm, LoginForm
 from flask_sqlalchemy import SQLAlchemy
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = '11f0c9648013c9a24e29a1e3f8579585'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(20), unique = True, nullable = False)
+    email = db.Column(db.String(120), unique = True, nullable = False)
+    image_file = db.Column(db.String(20), nullable = False, default = 'default.jpg')
+    password = db.Column(db.String(60), nullable = False)
+    sentences = db.relationship('Sentence', backref = 'author', lazy = True)
+
+    def __repr__(self):
+        return f"User('{ self.username }, '{ self.email }', '{ self.image_file }')"
+
+class Sentence(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    sentence = db.Column(db.Text, nullable = False)
+    entity_1 = db.Column(db.Text, nullable = False)
+    entity_2 = db.Column(db.Text, nullable = False)
+    relation = db.Column(db.Text, nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+
+    def __repr__(self):
+        return f"Sentence('{ self.sentence }','{ self.entity_1 }','{ self.entity_2 }','{ self.relation }')"
 
 sentences = [
     {
+        'author':'Dr. Nabeel Mohammed',
         'sentence':'He is a student',
         'entity_1': 'He',
         'entity_2' : 'student',
         'relation': 'is_a'
     },
     {
+        'author':'Dr. Nur Alam',
         'sentence':'Nur won the match',
         'entity_1': 'Nur',
         'entity_2' : 'the match',
         'relation': 'job_done'
     },
     {
+        'author':'Dr. Abdur Rahman',
         'sentence':'Student failed in the exam',
         'entity_1': 'Student',
         'entity_2' : 'failed in the exam',
