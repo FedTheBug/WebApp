1f5e1d060f341f10d2367e8ef79f529e861dda59
FedTheBug <fahadmuntasir747@gmail.com>
2019-12-07 21:31:09 +0600

    User Authentication

--- app/routes.py
+++ app/routes.py
@@ -1,40 +1,42 @@
-from flask import render_template, url_for, request, jsonify, flash, redirect
-from app import app
+from flask import render_template, url_for, request, jsonify, flash, redirect, request
+from app import app, db, bcrypt
 from app.forms import RegistrationForm, LoginForm
 from app.models import User, Sentence
+from flask_login import login_user, current_user, logout_user, login_required
 
 sentences = [
     {
-        'author':'Dr. Nabeel Mohammed',
-        'sentence':'He is a student',
-        'entity_1': 'He',
-        'entity_2' : 'student',
+        'author':'ডঃ নাবিল মোহাম্মেদ',
+        'sentence':'সে একজন ছাত্র',
+        'entity_1': 'সে',
+        'entity_2' : 'ছাত্র',
         'relation': 'is_a'
     },
     {
-        'author':'Dr. Nur Alam',
-        'sentence':'Nur won the match',
-        'entity_1': 'Nur',
-        'entity_2' : 'the match',
+        'author':'ডঃ নুর আলম',
+        'sentence':'রহিম ম্যাচটি জিতেছে',
+        'entity_1': 'রহিম',
+        'entity_2' : 'ম্যাচটি',
         'relation': 'job_done'
     },
     {
-        'author':'Dr. Abdur Rahman',
-        'sentence':'Student failed in the exam',
-        'entity_1': 'Student',
-        'entity_2' : 'failed in the exam',
-        'relation': 'job_done'
+        'author':'আবদুল মালেক',
+        'sentence':'তার একটি বাড়ি আছে',
+        'entity_1': 'তার',
+        'entity_2' : 'একটি বাড়ি',
+        'relation': 'has_a'
     }
 ]
 
 
 @app.route('/')
-def intro():
-    return render_template('intro.html')
-
 @app.route('/home')
 def home():
-    return render_template('home.html',sentences = sentences)
+    return render_template('home.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html',sentences = sentences)
 
 
 @app.route('/form')
@@ -51,19 +53,39 @@ def my_form_get():
 
 @app.route("/register", methods = ['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect (url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email = form.email.data, password = hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Your account created has been created! You are now able to log in', 'success')
+        return redirect(url_for('dashboard'))
     return render_template('register.html', title = 'Register', form = form)
 
 @app.route("/login",methods = ['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect (url_for('dashboard'))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been loggen in!','success')
-            return redirect(url_for('home'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember = form.remember.data)
+            next_page = request.args.get('next')
+            return redirect (next_page) if next_page else redirect(url_for('dashboard'))
         else:
             flash('Login Unsuccessful. Please check username and password','danger')
     return render_template('login.html', title = 'Login', form = form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title = 'Account')
